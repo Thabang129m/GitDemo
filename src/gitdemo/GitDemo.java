7aa6caffb95caba3f8aa5dba676340583855692c
Student <Student@ZAPRE-D00000160.DirectDelivery.com>
2017-10-11 11:07:36 +0200

    Give it a name

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,19 @@
+
+package gitdemo;
+
+
+public class GitDemo {
+
+    
+    public int add(String s1, String s2)
+    {
+        
+        
+        return 0;
+    }
+    
+    static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
